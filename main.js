3ad3bd319078220cc6a5333b622fae91a1a908bc
kirirotha <kirirotha@gmail.com>
2020-08-30 01:30:57 -0500

    Done.

--- main.js
+++ main.js
@@ -1,10 +1,42 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+document.querySelector('#modal').className += 'hidden'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  removeError();
+  clickHeart();
+});
 
+const removeError = () => {
+  document.querySelector('#modal').className += 'hidden'
+}
 
+const clickHeart = () =>{
+  document.querySelectorAll('.like').forEach( x => {
+    x.addEventListener('click', () => {
+      console.log('click')
+      let promise1 = mimicServerCall()
+      promise1.then(() =>{
+        if (x.innerText[6] === EMPTY_HEART) {
+          x.innerText = `Like! ${FULL_HEART}`
+          x.classList.add("activated-heart")
+        }else{
+          x.innerText = `Like! ${EMPTY_HEART}`
+          x.classList.remove("activated-heart")
+        }
+      })
+      promise1.catch((error) => {
+        console.log(error)
+        document.querySelector('#modal').classList.remove("hidden")
+        setTimeout(() => {
+        document.querySelector('#modal').classList.add("hidden")
+        }, 5000)
+      });
+    })
+  })
+} 
 
 
 //------------------------------------------------------------------------------
